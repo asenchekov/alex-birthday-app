64b62a575cd78a3909471f7b4f82723606a97c55
Asen Chekov <asen.chekov@gmail.com>
2021-09-25 17:05:52 +0100

    Fix: works for every year

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { useState, useEffect } from 'react';
-import logo from './cake_logo.svg';
-import background from './background.jpg';
-import fireworks from './fireworks-background.gif';
-import './App.css';
+import { useState, useEffect } from "react";
+import logo from "./cake_logo.svg";
+import background from "./background.jpg";
+import fireworks from "./fireworks-background.gif";
+import "./App.css";
 
 function App() {
-  const birthayDate = new Date('2021-01-15');
+  const year = new Date().getFullYear() + 1 + "";
+  const birthayDate = new Date(`${year}-01-15`);
 
-  const [ today, setToday ] = useState(new Date());
+  const [today, setToday] = useState(new Date());
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setToday(new Date())
+      setToday(new Date());
     }, 5000);
 
     return () => clearInterval(interval);
@@ -21,27 +22,46 @@ function App() {
   const hoursLeft = Math.floor((birthayDate - today) / 1000 / 60 / 60);
   const notYet = (birthayDate - today) / 1000 / 60 / 60 / 24;
 
-  const header = notYet > 0
-    ? (
+  const header =
+    notYet > 0 ? (
       <>
-        {daysLeft
-          ? <h1>{daysLeft} {daysLeft !== 1 ? 'Days' : 'Day'}</h1>
-          : <h1>{hoursLeft} {hoursLeft !== 1 ? 'Hours' : 'Hour'}</h1>}
-        <h3>left to Alexandra Chekova's Birthday</h3>
+        {daysLeft ? (
+          <h1>
+            {daysLeft} {daysLeft !== 1 ? "Days" : "Day"}
+          </h1>
+        ) : (
+          <h1>
+            {hoursLeft} {hoursLeft !== 1 ? "Hours" : "Hour"}
+          </h1>
+        )}
+        <h3>left to Alex Chekova's Birthday</h3>
       </>
-    )
-    : <h1>It's your Birthday!!!</h1>;
+    ) : (
+      <h1>It's your Birthday!!!</h1>
+    );
 
   return (
     <div className="App">
-      <header className="App-header"
+      <header
+        className="App-header"
         style={{
-          background: `center no-repeat url(${notYet > 0 ? background : fireworks})`,
-          backgroundSize: 'cover',
-          backgroundColor: '#282c34',
+          background: `center no-repeat url(${
+            notYet > 0 ? background : fireworks
+          })`,
+          backgroundSize: "cover",
+          backgroundColor: "#282c34",
         }}
       >
         <img src={logo} className="App-logo" alt="logo" />
+        <iframe
+          title="unique"
+          width="560"
+          height="320"
+          src="https://musiclab.chromeexperiments.com/Song-Maker/embed/6118495637471232"
+          frameborder="0"
+          allowfullscreen
+        />
+
         {header}
       </header>
     </div>
